36b52e7dbed4fa44b2595c846510a27e98906c0f
Mark Harland <markharland75@gmail.com>
2021-11-20 22:17:11 -0800

    Clear button functionality now working

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 // Select items
 const alertBox = document.getElementById("alert");
 const form = document.querySelector(".grocery-list-form");
-const inputGrocery = document.getElementById("input-grocery"); 
+const inputGrocery = document.getElementById("input-grocery");
 const submitBtn = document.querySelector(".submit-btn");
 const editBtn = document.querySelector(".edit-btn");
 const deleteBtn = document.querySelector(".remove-btn");
 const listOfItems = document.querySelector(".grocery-items-container");
+const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
 //declare variables to help edit list later on
 
 // Event Listeners
 form.addEventListener("submit", addItem);
+clearBtn.addEventListener("click", clearList);
+//deleteBtn.addEventListener("click", deleteItem); //CANT DO THIS... ITEMS ADDED DYNAMICALLY
 
 //for editing items
 let editElement;
@@ -19,50 +22,85 @@ let editFlag;
 let editID;
 
 // Functions
-function addItem(e){
-    e.preventDefault();
-    const groceryItemInput = inputGrocery.value;
-    const id = new Date().getTime().toString();
-    //  account for all conditions of input; 
-    if(groceryItemInput && !editFlag){   //input case: input is not empty and user NOT editing
-        //add item to the grocery list
-        const newElement = document.createElement("article");
-        newElement.classList.add("grocery-item");
+function addItem(e) {
+  e.preventDefault();
+  const groceryItemInput = inputGrocery.value;
+  const id = new Date().getTime().toString();
+  //  account for all conditions of input;
+  if (groceryItemInput && !editFlag) {
+    //input case: input is not empty and user NOT editing
+    //add item to the grocery list
+    const newElement = document.createElement("article");
+    newElement.classList.add("grocery-item");
 
-        //creates a new attribute to be considered for use in the whole document
-        const attrID = document.createAttribute('data-id');
-        attrID.value = id;
+    //creates a new attribute to be considered for use in the whole document
+    const attrID = document.createAttribute("data-id");
+    attrID.value = id;
 
-        //sets the specific grocery item's created ID attribute 
-        newElement.setAttributeNode(attrID);
-        newElement.innerHTML = `<p class="grocery-title">${groceryItemInput}</p>
-                                <div class="item-btn-container">
-                                    <button type="button" class="edit-btn">edit</button>
-                                    <button type="button" class="remove-btn">del.</button>
-                                </div>`;
-        listOfItems.appendChild(newElement);
-        displayAlert("Item added successfully", "success");
-    } else if(groceryItemInput && editFlag) { //input case: input is not empty and user IS editing
-        //edit the option selected
-        
-    } else { //empty input
-        displayAlert("Please enter a value", "danger");
-    }
+    //sets the specific grocery item's created ID attribute
+    newElement.setAttributeNode(attrID);
+    newElement.innerHTML = `    <p class="grocery-title">${groceryItemInput}</p>
+                                    <div class="item-btn-container">
+                                        <button type="button" class="edit-btn">edit</button>
+                                        <button type="button" class="remove-btn">del.</button>
+                                    </div>`;
+    listOfItems.appendChild(newElement);
+    clearBtn.classList.add("show-container");
+    displayAlert("Item added successfully", "success");
+
+    //adding to local storage
+    addToLocalStorage(id, inputGrocery);
+    setBackToDefault();
+  } else if (groceryItemInput && editFlag) {
+    //input case: input is not empty and user IS editing
+    //edit the option selected
+  } else {
+    //empty input
+    displayAlert("Please enter a value", "danger");
+  }
 }
 
-
 //display alert
-function displayAlert(text, typeOfAlert){
-    alertBox.textContent = text;
-    alertBox.classList.add(`alert-${typeOfAlert}`);
-    
-    setTimeout(function(){
-        alertBox.textContent = '';
-        alertBox.classList.remove(`alert-${typeOfAlert}`);
-    }, 3000);
-    
+function displayAlert(text, typeOfAlert) {
+  alertBox.textContent = text;
+  alertBox.classList.add(`alert-${typeOfAlert}`);
+
+  setTimeout(function () {
+    alertBox.textContent = "";
+    alertBox.classList.remove(`alert-${typeOfAlert}`);
+  }, 3000);
+}
+
+//set back to default
+function setBackToDefault() {
+  //placerholder
+  console.log("Set to default");
+  inputGrocery.value = "";
+  editFlag = false;
+  editID = "";
+  submitBtn.textContent = "Add Item";
+}
+
+//clear all items from grocery list
+function clearList() {
+  const items = document.querySelectorAll(".grocery-item"); //returns array
+
+  //in items, loop through, store in a variable called item for each loop, then remove that child based on
+  if (items.length > 0) {
+    items.forEach(function (item) {
+      listOfItems.removeChild(item);
+    });
+  }
+
+  clearBtn.classList.remove("show-container");
+  displayAlert("Items cleared", "danger");
+  setBackToDefault();
 }
 
 // Local Storage
+function addToLocalStorage() {
+  //placeholder
+  console.log("Add to local storage");
+}
 
 // Setup Items
